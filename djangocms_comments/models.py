afe32b63ba7c9b69dbb41906bfefeb940ca3cd25
desinutrition <dev@desinutrition.ca>
2024-06-29 13:38:13 -0400

    "Add on_delete argument to ForeignKey fields for Django 4.x and 5.x compatibility"

--- djangocms_comments/models.py
+++ djangocms_comments/models.py
@@ -37,9 +37,6 @@ class CommentsConfig(models.Model):
 
 class CommentsCMSPlugin(CMSPlugin):
     """AppHookConfig aware abstract CMSPlugin class for Aldryn Newsblog"""
-    # avoid reverse relation name clashes by not adding a related_name
-    # to the parent plugin
-
     config = models.ForeignKey(CommentsConfig, on_delete=models.CASCADE)
 
     class Meta:
@@ -50,7 +47,7 @@ class CommentsCMSPlugin(CMSPlugin):
 
 
 class Comment(models.Model):
-    config = models.ForeignKey(CommentsConfig)
+    config = models.ForeignKey(CommentsConfig, on_delete=models.CASCADE)
 
     page_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, related_name='+')
     page_id = models.PositiveIntegerField()
@@ -65,10 +62,9 @@ class Comment(models.Model):
     requires_attention = models.CharField(blank='', max_length=16, choices=REQUIRES_ATTENTION)
     moderated = models.CharField(blank='', max_length=16, choices=MODERATED)
     moderated_reason = models.CharField(max_length=120, blank=True)
-    moderated_by = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
+    moderated_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
 
     user_ip = models.GenericIPAddressField()
-    # http://stackoverflow.com/questions/654921/how-big-can-a-user-agent-string-get
     user_agent = models.TextField()
     referrer = models.URLField(blank=True)
     published = models.BooleanField(default=False)
@@ -80,7 +76,7 @@ class Comment(models.Model):
         return self.updated_at.replace(second=0, microsecond=0) > self.created_at.replace(second=0, microsecond=0)
 
     def __str__(self):
-        return u'[{}] {}'.format(self.page, truncatechars(self.body, 40))
+        return '[{}] {}'.format(self.page, truncatechars(self.body, 40))
 
     def __unicode__(self):
         return self.__str__()
@@ -90,9 +86,10 @@ class AnonymousAuthor(models.Model):
     username = models.CharField(max_length=32)
     email = models.EmailField()
     website = models.URLField(blank=True)
-    comments = GenericRelation(Comment, object_id_field='author_id', content_type_field='author_type',
-                               **{('related_name' if django.VERSION < (1, 7)
-                                   else 'related_query_name'): 'anonymous_authors'})
+    comments = GenericRelation(
+        Comment, object_id_field='author_id', content_type_field='author_type',
+        **{'related_query_name' if django.VERSION >= (1, 7) else 'related_name': 'anonymous_authors'}
+    )
 
     def __str__(self):
         return self.username

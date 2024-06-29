79b680cb5f6f654a974f5f9d170a0ffe110b9aef
desinutrition <dev@desinutrition.ca>
2024-06-29 13:51:12 -0400

    Update fields.py - Update fields.py for Django 4.x and 5.x compatibility

--- djangocms_comments/fields.py
+++ djangocms_comments/fields.py
@@ -2,24 +2,20 @@ import django
 from django import forms
 from django.forms.fields import ChoiceField
 from django.forms.utils import flatatt
-from django.forms.widgets import CheckboxChoiceInput, ChoiceFieldRenderer, RadioChoiceInput, SubWidget, RendererMixin, \
-    Select, CheckboxInput
+from django.forms.widgets import ChoiceInput, Select, CheckboxInput
 from django.utils import html
+from django.utils.encoding import force_text
+from django.utils.html import format_html
 
-
-from django.utils.encoding import python_2_unicode_compatible, force_text
 if django.VERSION < (1, 7):
     from django.utils.html import html_safe
 else:
     def html_safe(klass):
         return klass
-from django.utils.html import format_html
-
 
 class SubmitButtonWidget(forms.Widget):
     def render(self, name, value, attrs=None):
         return '<input type="submit" name="%s" value="%s">' % (html.escape(name), html.escape(value))
-
 
 class SubmitButtonField(forms.Field):
     def __init__(self, *args, **kwargs):
@@ -32,46 +28,36 @@ class SubmitButtonField(forms.Field):
     def clean(self, value):
         return value
 
-
-class ButtonGroupRenderer(ChoiceFieldRenderer):
+class ButtonGroupRenderer:
     outer_html = '<div class="btn-group" role="group">{content}</div>'
     inner_html = '{choice_value}{sub_widgets}'
 
-
-@html_safe
-@python_2_unicode_compatible
-class ChoiceButton(SubWidget):
-    input_type = None  # Subclasses must define this
-
-    def __init__(self, name, value, attrs, choice, index):
-        self.choice_value = force_text(choice[0])
-        self.choice_label = force_text(choice[1])
-        attrs = dict(attrs)
-        enabled_class = attrs.pop('enabled_classes', {}).get(self.choice_value, 'btn-primary')
-        disabled_class = attrs.pop('dissabled_classes', {}).get(self.choice_value, '')
+    def __init__(self, name, value, attrs, choices):
         self.name = name
         self.value = value
         self.attrs = attrs
-        self.index = index
-        if 'id' in self.attrs:
-            self.attrs['id'] += "_%d" % self.index
-        self.attrs['type'] = self.attrs.get('type', 'submit')
+        self.choices = choices
+
+    def render(self):
+        output = []
+        for index, (option_value, option_label) in enumerate(self.choices):
+            w = ChoiceButton(self.name, self.value, self.attrs.copy(), (option_value, option_label), index)
+            output.append(w.render())
+        return format_html(self.outer_html.format(content=mark_safe('\n'.join(output))))
+
+@html_safe
+class ChoiceButton(ChoiceInput):
+    input_type = None  # Subclasses must define this
+
+    def __init__(self, name, value, attrs, choice, index):
+        super().__init__(name, value, attrs, choice, index)
+        self.choice_value = force_text(choice[0])
+        self.choice_label = force_text(choice[1])
+        enabled_class = attrs.pop('enabled_classes', {}).get(self.choice_value, 'btn-primary')
+        disabled_class = attrs.pop('dissabled_classes', {}).get(self.choice_value, '')
         self.attrs['class'] = ' '.join(filter(lambda x: x, [self.attrs.get('class', 'btn'),
                                                             enabled_class if self.value == self.choice_value
                                                             else disabled_class]))
-
-    def __str__(self):
-        return self.render()
-
-    def render(self, name=None, value=None, attrs=None, choices=()):
-        if self.id_for_label:
-            label_for = format_html(' for="{}"', self.id_for_label)
-        else:
-            label_for = ''
-        return format_html(self.tag(attrs))
-
-    def is_checked(self):
-        return True
 
     def tag(self, attrs=None):
         attrs = attrs or self.attrs
@@ -80,19 +66,12 @@ class ChoiceButton(SubWidget):
             final_attrs['checked'] = 'checked'
         return format_html('<button{}>{}</button>', flatatt(final_attrs), self.choice_label)
 
-    @property
-    def id_for_label(self):
-        return self.attrs.get('id', '')
-
-
 class MultipleSubmitButtonRendered(ButtonGroupRenderer):
     choice_input_class = ChoiceButton
 
-
-class MultipleSubmitButton(RendererMixin, Select):
+class MultipleSubmitButton(Select):
     renderer = MultipleSubmitButtonRendered
     _empty_value = ''
-
 
 class Button(CheckboxInput):
     def __init__(self, attrs=None, check_test=None, values=None):
